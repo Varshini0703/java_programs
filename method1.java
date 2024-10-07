bbb7d9cfaf0d02b5ba7a009e2c005368ed4deec5
Varshini0703 <153826159+Varshini0703@users.noreply.github.com>
2024-10-07 22:21:57 +0530

    Add files via upload

---
+++ method1.java
@@ -0,0 +1,14 @@
+public class method1 {
+
+    public int method(){
+        int x = 5;
+        int y = 70;
+        return x+y;
+    }
+
+    public static void main(String[] args){
+        method1 obj = new method1();
+        System.out.println(obj.method()); 
+    }
+}
+
---
+++ sum_natural.java
@@ -0,0 +1,9 @@
+public class sum_natural {
+    public static void main(String[] args){
+        int i,sum=0;
+        for(i=1;i<11;i++){
+            sum = sum+i;
+        }
+        System.out.println(sum);
+    } 
+}
